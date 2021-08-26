55e39ec83559801650418dbcb022494b206a7cbf
JacobinaByjus <89422919+JacobinaByjus@users.noreply.github.com>
2021-08-26 16:40:13 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var trex, trexRunning, edges;
+var groundImage;
+
+function preload() {
+  trexRunning = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  groundImage = loadImage("ground2.png");
+}
+
+function setup() {
+  createCanvas(600, 200);
+
+  //criando o trex
+  trex = createSprite(50, 160, 20, 50);
+  trex.addAnimation("running", trexRunning);
+  edges = createEdgeSprites();
+
+  //adicione dimensão e posição ao trex
+  trex.scale = 0.5;
+  trex.x = 50;
+}
+
+function draw() {
+  //definir a cor do plano de fundo
+  background("white");
+
+  //registrando a posição y do trex
+  console.log(trex.y);
+
+  //pular quando tecla de espaço for pressionada
+  if (keyDown("space")) {
+    trex.velocityY = -10;
+  }
+
+  trex.velocityY = trex.velocityY + 0.5;
+
+  //impedir que o trex caia
+  trex.collide(edges[3]);
+  drawSprites();
+}
